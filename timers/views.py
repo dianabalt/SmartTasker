e11e78866c58d70cc4ef0d150c85ee9aec83764f
skaya363 <ksude3837@gmail.com>
2025-07-15 16:26:22 -0400

    Update views.py

--- timers/views.py
+++ timers/views.py
@@ -1,30 +1,3 @@
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
-from .models import Timer
-from django.utils.timezone import now
+from django.shortcuts import render
 
-def start_timer(request, timer_id):
-    timer = get_object_or_404(Timer, id=timer_id)
-    timer.is_active = True
-    timer.is_paused = False
-    timer.started_at = now()
-    timer.save()
-    return JsonResponse({'status': 'started'})
-
-def pause_timer(request, timer_id):
-    timer = get_object_or_404(Timer, id=timer_id)
-    if timer.is_active and not timer.is_paused:
-        timer.is_paused = True
-        timer.paused_at = now()
-        # Optionally calculate time spent so far
-        timer.save()
-    return JsonResponse({'status': 'paused'})
-
-def stop_timer(request, timer_id):
-    timer = get_object_or_404(Timer, id=timer_id)
-    timer.is_active = False
-    timer.is_paused = False
-    # Optionally finalize time tracking
-    timer.save()
-    return JsonResponse({'status': 'stopped'})
-
+# Create your views here
