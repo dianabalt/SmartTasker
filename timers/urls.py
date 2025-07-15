351ae0119daf96098ae4998962921253e538be9c
skaya363 <ksude3837@gmail.com>
2025-07-15 14:58:19 -0400

    Update urls.py

--- timers/urls.py
+++ timers/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # Add timer-related URL patterns here
+    path('start/<int:timer_id>/', views.start_timer, name='start_timer'),
+    path('pause/<int:timer_id>/', views.pause_timer, name='pause_timer'),
+    path('stop/<int:timer_id>/', views.stop_timer, name='stop_timer'),
 ]
+
